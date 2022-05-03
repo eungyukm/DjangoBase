9ffdb3bce7423e577f2e6f51f954ec89059abafd
eungyukm <86348187+eungyukm@users.noreply.github.com>
2022-05-03 10:25:39 +0900

    upload

--- django/myworld/myworld/urls.py
+++ django/myworld/myworld/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # memebers.urls에 작성한 주소 매핑 정보를 등록합니다.
+    path('members/', include('members.urls'))
 ]
